7e9c092a4d909e3c1daa100840e19c31ce4bfd87
amir taghipour <amir_fco@yahoo.com>
2024-01-29 21:23:33 +0330

    config mainActivity request location permission request location updates

--- app/src/main/java/com/uber/ubercustomer/activity/MainActivity.java
+++ app/src/main/java/com/uber/ubercustomer/activity/MainActivity.java
@@ -1,16 +1,67 @@
 package com.uber.ubercustomer.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import com.uber.ubercustomer.R;
+import com.uber.ubercustomer.tools.IActivityBase;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements IActivityBase {
+
+    private LocationManager locationManager;
+    private LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestLocationPermission();
+        initViewRefrences();
+        initViewActions();
+        initInitialValues();
+    }
+
+    @Override
+    public void initViewRefrences() {
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+
+            }
+        };
+    }
+
+    @Override
+    public void initViewActions() {
+        setLocationUpdateListener();
+    }
+
+    @Override
+    public void initInitialValues() {
+
+    }
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    private void requestLocationPermission() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+        }
+    }
+
+    private void setLocationUpdateListener() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+        )
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, locationListener);
     }
 }
